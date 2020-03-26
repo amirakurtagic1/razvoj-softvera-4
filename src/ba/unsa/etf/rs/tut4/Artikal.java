49a1750c2175779cfb3bfdc243408d09e5dcc39c
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2020-03-26 15:37:13 +0100

    Kreirane metode: toString, izbaciDuplikate, equals; Kreiran kontruktor sa jednim parametrom;

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -1,4 +1,101 @@
 package ba.unsa.etf.rs.tut4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Artikal {
+    private String sifra;
+    private String naziv;
+    private double cijena;
+ //  private  ArrayList<Artikal> lista = new ArrayList<Artikal>();
+
+
+    public Artikal() {}
+
+    public Artikal(String sifra, String naziv, double cijena) {
+        setSifra(sifra);
+        setNaziv(naziv);
+        setCijena(cijena);
+    }
+
+    public Artikal(String artikal) {
+        String[] atributi = artikal.split(",");
+        sifra = atributi[0];
+        naziv = atributi[1];
+        cijena = Double.parseDouble(atributi[2]);
+       setSifra(sifra);
+        setNaziv(naziv);
+        setCijena(cijena);
+    }
+
+    public static ArrayList<Artikal> izbaciDuplikate(ArrayList<Artikal> lista)
+    {
+        ArrayList<Artikal> lista1 = new ArrayList<>();
+        for(int i = 0; i < lista.size(); i++)
+        {
+            for(int j = i+1; j < lista.size(); j++)
+            {
+
+               if (lista.get(i).equals(lista.get(j))) lista.remove(j);
+               lista1.add(lista.get(i));
+            }
+        }
+        return lista1;
+    }
+
+    public String getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(String sifra) {
+        if(sifra.isEmpty() || sifra.length() == 0) throw new IllegalArgumentException("Šifra je prazna");
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        if(naziv.isEmpty() || naziv.length() == 0) throw new IllegalArgumentException("Naziv je prazan");
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        if(cijena <= 0) throw new IllegalArgumentException("Cijena je negativna");
+        this.cijena = cijena;
+    }
+
+    @Override
+    public String toString() {
+        return sifra + "," + naziv + "," + cijena;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(!(o instanceof Artikal)) return false;
+        Artikal a = (Artikal)o;
+        if(!this.sifra.equals(a.sifra) || !this.naziv.equals(a.naziv) || this.cijena != a.cijena) return false;
+        return true;
+    }
+
+   /* public static void IzbaciDuplikate(ArrayList<Artikal> artikli)
+    {
+        for(int i = 0; i < artikli.size(); i++)
+        {
+           for(int j = i+1; j < artikli.size(); j++)
+            {
+                if(artikli.get(i).equals(artikli.get(j))) artikli.remove(j);
+                j--;
+            }
+        }
+    }*/
+
+
+
+
 }
--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -1,7 +1,27 @@
 package ba.unsa.etf.rs.tut4;
 
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sretan rad na tutorijalu 4!");
+      try {
+          ArrayList<Artikal> lista = new ArrayList<>();
+          Artikal artikal = new Artikal("Nesto,naziv,2");
+          lista.add(artikal);
+          Artikal art1 = new Artikal("Nesto,naziv,3.5");
+          lista.add(art1);
+          Artikal art2 = new Artikal("Nesto", "naziv", 3.5);
+          lista.add(art2);
+
+          Artikal.izbaciDuplikate(lista);
+
+          for(int i = 0; i < lista.size(); i++)
+          {
+              System.out.println(lista.get(i));
+          }
+      } catch(Exception e){
+          System.out.println(e.getMessage());
+      }
     }
 }
