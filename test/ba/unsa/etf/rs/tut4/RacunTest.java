62caadd28592c3d42d003e919038beb2a4291623
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2020-04-01 21:53:29 +0200

    Dodati testovi za prekrivenost klase Racun

--- test/ba/unsa/etf/rs/tut4/RacunTest.java
+++ test/ba/unsa/etf/rs/tut4/RacunTest.java
@@ -19,4 +19,31 @@ class RacunTest {
         Racun r = new Racun();
         assertEquals(0, r.ukupanIznos());
     }
+
+    @Test
+    void getArtikal() {
+        Racun.StavkaRacuna racun = new Racun.StavkaRacuna(new Artikal("HLB", "Hljeb", 1.1), 2);
+        assertEquals(new Artikal("HLB", "Hljeb", 1.1), racun.getArtikal());
+    }
+
+    @Test
+    void setArtikal() {
+        Racun.StavkaRacuna racun = new Racun.StavkaRacuna(new Artikal("HLB", "Hljeb", 1.1), 2);
+        racun.setArtikal(new Artikal("Dr","Drugo",1.2));
+        assertEquals(new Artikal("Dr", "Drugo", 1.2), racun.getArtikal());
+
+    }
+
+    @Test
+    void getKolicina() {
+        Racun.StavkaRacuna racun = new Racun.StavkaRacuna(new Artikal("HLB", "Hljeb", 1.1), 2);
+        assertEquals(2, racun.getKolicina());
+    }
+
+    @Test
+    void setKolicina() {
+        Racun.StavkaRacuna racun = new Racun.StavkaRacuna(new Artikal("HLB", "Hljeb", 1.1), 2);
+        racun.setKolicina(3);
+        assertEquals(3, racun.getKolicina());
+    }
 }
