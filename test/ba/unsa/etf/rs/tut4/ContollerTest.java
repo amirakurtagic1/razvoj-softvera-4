3e6f7101faa0c96e00354fc3ce415554e1083028
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2020-04-02 01:06:26 +0200

    Nije dovrsen test: IspitivanjeAktuelnogRacuna

--- test/ba/unsa/etf/rs/tut4/ContollerTest.java
+++ test/ba/unsa/etf/rs/tut4/ContollerTest.java
@@ -1,9 +1,12 @@
 package ba.unsa.etf.rs.tut4;
 
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -78,7 +81,36 @@ class ContollerTest {
         assertEquals("Sifra2", artikal.getSifra());
         assertEquals("Proizvod2", artikal.getNaziv());
         assertEquals(2.0, artikal.getCijena());
-
+    }
+    @Test
+    void IspitivanjeAktuelnogRacuna(FxRobot robot){
+        Button button = robot.lookup("#button").queryButton();
+        assertNotNull(button);
+        robot.clickOn("#Unos");
+        robot.write("Sifra1,Proizvod1,1\nSifra2,Proizvod2,2\nSifra3,Proizvod3,3\n");
+        robot.clickOn(button);
+        robot.clickOn("#Tab2");
+        ChoiceBox item = robot.lookup("#item").queryAs(ChoiceBox.class);
+        robot.clickOn("#item");
+        Platform.runLater(() -> item.show());
+        try{
+            Thread.sleep(200);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        robot.clickOn("Sifra2,Proizvod2,2.0");
+        Spinner spinner = robot.lookup("#kolicina").queryAs(Spinner.class);
+        robot.clickOn("kolicina");
+        try{
+            Thread.sleep(200);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        robot.write("2");
+        robot.clickOn("#Dodaj");
+        TextArea racun = robot.lookup("#racun").queryAs(TextArea.class);
+        Racun racun1 = new Racun();
+        racun1.dodajStavku(new Artikal("HLB", "Hljeb", 1.1), 2);
 
 
     }
